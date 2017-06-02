bbe4b088e3e19de801aaf85c11e4dfe55eb38a4a
Amir Halperin <amiram.halperin@gmail.com>
2017-06-02 13:42:57 +0300

    add map component container

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,17 +5,22 @@ import { MyZoneComponent } from './my-zone/my-zone.component';
 import { SearchesComponent } from './my-zone/searches/searches.component';
 import { SketchesComponent } from './my-zone/sketches/sketches.component';
 import { TagsComponent } from './my-zone/tags/tags.component';
+import { MapComponent } from './map/map.component';
 import { AppComponent } from './app.component';
 
 const appRoutes: Routes = [
-  { path: 'my-zone', component: MyZoneComponent, children: [
-      { path: 'searches', component: SearchesComponent },
-      { path: 'tags', component: TagsComponent },
-      { path: 'sketches', component: SketchesComponent },
-      { path: '', redirectTo: 'searches', pathMatch: 'full' }
+    { path: '', redirectTo: 'map', pathMatch: 'full' },
+    { path: 'map', component: MapComponent, children:[
+      { path: '', redirectTo: 'my-zone', pathMatch: 'full'},
+      { path: 'my-zone', component: MyZoneComponent, children: [
+          { path: '', redirectTo: 'searches', pathMatch: 'full' },
+          { path: 'searches', component: SearchesComponent },
+          { path: 'tags', component: TagsComponent },
+          { path: 'sketches', component: SketchesComponent }
+        ] 
+      }
     ] 
-  },
-  { path: '', redirectTo: 'my-zone', pathMatch: 'full'}   
+  }
 ];
 
 @NgModule({
